9ed64b8dd715ffde4bc8fedc956bb5ff773a4bf9
KorolevAV <antoshka.992@mail.ru>
2022-04-18 00:44:31 +0300

    edit game.py, add 'HAHAHA'

--- game.py
+++ game.py
@@ -43,41 +43,42 @@ def game(update: Update, context: CallbackContext):
                 word_user = gw.word_competion(reslist[1], censured_word, word)
                 censured_word = word_user
                 if censured_word.find('*') != -1:
-                    update.message.reply_text(f'{censured_word}, please enter /game (space) a letter you want to guess')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                 else:
-                    update.message.reply_text('Congratulations! You are win!')
+                    update.message.reply_text(f'{censured_word}\nCongratulations! You are win!')
                 break
             else:
-                update.message.reply_text(f'Your letter ({reslist[1]}) is NOT inside your word')
+                update.message.reply_text(f'Your letter ({reslist[1]}) is NOT inside hidden word')
                 count = count - 1
                 if count == 7:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_142913.jpg?raw=true')
-                    update.message.reply_text(f'{censured_word}, please enter /game (space) a letter you want to guess')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                     break
                 elif count == 6:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_142928.jpg?raw=true')
-                    update.message.reply_text(f'{censured_word}, please enter /game (space) a letter you want to guess')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                     break
                 elif count == 5:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_143033.jpg?raw=true')
-                    update.message.reply_text(f'{censured_word}, please enter /game (space) a letter you want to guess')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                     break
                 elif count == 4:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_143053.jpg?raw=true')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                     break
                 elif count == 3:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_143123.jpg?raw=true')
-                    update.message.reply_text(f'{censured_word}, please enter /game (space) a letter you want to guess')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                     break
                 elif count == 2:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_143150.jpg?raw=true')
-                    update.message.reply_text(f'{censured_word}, please enter /game (space) a letter you want to guess')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                     break
                 elif count == 1:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_143211.jpg?raw=true')
-                    update.message.reply_text(f'{censured_word}, please enter /game (space) a letter you want to guess')
+                    update.message.reply_text(f'{censured_word}, please, enter /game (space) a letter you want to guess')
                     break
                 elif count == 0:
                     update.message.reply_photo('https://github.com/Blakyyy/telega_bot/blob/main/20220415_143224.jpg?raw=true')
-                    update.message.reply_text('You dont have more lifes, you are hanged')
+                    update.message.reply_text('You dont have more lifes, you are hanged! HAHAHA!')
                     break
